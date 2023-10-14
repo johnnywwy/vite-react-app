357ea2a173c60d53ad1050db731d628ff5afd0fb
wuwenyu <johnnywwy@gmail.com>
2023-10-14 11:01:36 +0800

    feat：引入全局样式文件

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+// 全局样式
+import '@/common/styles/frame.styl'
 
 ReactDOM.createRoot(
   document.getElementById('root')!).render(
