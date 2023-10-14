a77f5d735fa9eb3ce38277654a534f9015b13c55
wuwenyu <johnnywwy@gmail.com>
2023-10-14 12:01:57 +0800

    feat：引入全局路由

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+
+import { RouterProvider } from 'react-router-dom'
+
+import { globalRouters } from '@/router'
+
+// import App from './App'
+// import App from '@/pages/login'
+// import App from '@/pages/home'
+// import App from '@/pages/account'
+
 import { ConfigProvider } from 'antd'
 
 // 引入Ant Design中文语言包
@@ -9,9 +18,10 @@ import zhCN from 'antd/locale/zh_CN'
 // 全局样式
 import '@/common/styles/frame.styl'
 
-ReactDOM.createRoot(
-  document.getElementById('root')!).render(
-    <ConfigProvider locale={zhCN} >
-      <App />
-    </ConfigProvider >
+
+ReactDOM.createRoot(document.getElementById('root')!)
+  .render(
+    <ConfigProvider locale={zhCN}>
+      <RouterProvider router={globalRouters} />
+    </ConfigProvider>
   )
