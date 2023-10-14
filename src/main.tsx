f926921e9fb82141af3891d622e9e2c638a9a1e5
wuwenyu <johnnywwy@gmail.com>
2023-10-14 11:10:05 +0800

    feat：Antd修改默认语言为中文

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { ConfigProvider } from 'antd'
+
+// 引入Ant Design中文语言包
+import zhCN from 'antd/locale/zh_CN'
+
 // 全局样式
 import '@/common/styles/frame.styl'
 
 ReactDOM.createRoot(
   document.getElementById('root')!).render(
-    <React.StrictMode>
+    <ConfigProvider locale={zhCN} >
       <App />
-    </React.StrictMode>,
+    </ConfigProvider >
   )
