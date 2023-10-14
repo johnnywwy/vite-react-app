bbb5be0bcb2f8604fc1e3cb8e5be8f2bf79db99f
wuwenyu <johnnywwy@gmail.com>
2023-10-14 11:08:29 +0800

    feat：antd Button加载成功

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import { Button } from "antd"
+
 function App() {
-  return <div className="App">Vite-React-App</div>
+  return <div className="App">
+    <h1>Vite-React-App</h1>
+    <Button type="primary">Button</Button>
+  </div>
 }
 
 export default App
