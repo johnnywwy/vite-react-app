258fe1607166c95ae82f258ac53393f186ce4424
wuwenyu <johnnywwy@gmail.com>
2023-10-14 11:35:46 +0800

    feat：login页面基本完成

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,24 @@
+import { Button, Input } from 'antd'
+import imgLogo from './logo.svg'
+import './login.styl'
+
+function Login() {
+  return (
+    <div className="P-login">
+      <img src={imgLogo} alt="" className="logo" />
+      <div className="ipt-con">
+        <Input placeholder="账号" />
+      </div>
+      <div className="ipt-con">
+        <Input.Password placeholder="密码" />
+      </div>
+      <div className="ipt-con">
+        <Button type="primary" block={true}>
+          登录
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default Login
